caa0c6da1db326b683351198a6134217050046f5
Arthur Silveira <arthurmarques2148@gmail.com>
2025-10-12 19:49:38 -0300

    refactor: replace multiple Lombok annotations with @Data

--- src/main/java/academy/devdojo/springboot_essentials/domain/Anime.java
+++ src/main/java/academy/devdojo/springboot_essentials/domain/Anime.java
@@ -1,14 +1,11 @@
 package academy.devdojo.springboot_essentials.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
-@Getter
-@Setter
-@NoArgsConstructor
+@Data
 @AllArgsConstructor
 public class Anime {
+    // Representa o que temos do banco de dados
+    private Long id;
     private String name;
 }
