ba681e5d120cdb0cb39b2043b189b5bf1c0cdc1f
Arthur Silveira <arthurmarques2148@gmail.com>
2025-10-12 19:53:59 -0300

    feat(service): implement AnimeService with listAll and findById methods

---
+++ src/main/java/academy/devdojo/springboot_essentials/service/AnimeService.java
@@ -0,0 +1,25 @@
+package academy.devdojo.springboot_essentials.service;
+
+import academy.devdojo.springboot_essentials.domain.Anime;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@Service
+public class AnimeService {
+    // Responsavel pela lógica de négocio
+    // private final AnimeRepository animeRepository;
+    private List<Anime> animes = List.of(new Anime(1L, "DBZ"), new Anime(2L, "Berserk"));
+
+    public List<Anime> listAll()  {
+        return animes;
+    }
+    public Anime findById(long id)  {
+        return animes.stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
+    }
+}
