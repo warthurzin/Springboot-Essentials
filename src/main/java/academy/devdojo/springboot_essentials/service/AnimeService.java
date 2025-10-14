2faaad7e96ecb8a0e79568136d4361a6b87bd4dc
Arthur Silveira <arthurmarques2148@gmail.com>
2025-10-14 11:45:14 -0300

    feat(service): add save method to create new animes

--- src/main/java/academy/devdojo/springboot_essentials/service/AnimeService.java
+++ src/main/java/academy/devdojo/springboot_essentials/service/AnimeService.java
@@ -5,21 +5,34 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class AnimeService {
     // Responsavel pela lógica de négocio
     // private final AnimeRepository animeRepository;
-    private List<Anime> animes = List.of(new Anime(1L, "DBZ"), new Anime(2L, "Berserk"));
+    private static List<Anime> animes;
+
+    static {
+        animes = new ArrayList<>(List.of(new Anime(1L, "DBZ"), new Anime(2L, "Berserk")));
+    }
 
     public List<Anime> listAll()  {
         return animes;
     }
+
     public Anime findById(long id)  {
         return animes.stream()
                 .filter(anime -> anime.getId().equals(id))
                 .findFirst()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
+    }
+
+    public Anime save(Anime anime) {
+        anime.setId(ThreadLocalRandom.current().nextLong(3,1000000));
+        animes.add(anime);
+        return anime;
     }
 }
