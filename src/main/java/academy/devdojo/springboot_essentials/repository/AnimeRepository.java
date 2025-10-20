f114d6808574541771005584507498a49541e058
Arthur Silveira <arthurmarques2148@gmail.com>
2025-10-20 11:42:10 -0300

    feat(repository): integrate AnimeRepository with Spring Data JPA

--- src/main/java/academy/devdojo/springboot_essentials/repository/AnimeRepository.java
+++ src/main/java/academy/devdojo/springboot_essentials/repository/AnimeRepository.java
@@ -1,10 +1,10 @@
 package academy.devdojo.springboot_essentials.repository;
 
 import academy.devdojo.springboot_essentials.domain.Anime;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface AnimeRepository {
+public interface AnimeRepository extends JpaRepository<Anime, Long> {
     // Conexão com o banco de dados
-    List<Anime> listAll();
 }
