5814dcef343ee5502c88e3a81e6f96373803bb98
Arthur Silveira <arthurmarques2148@gmail.com>
2025-10-20 11:43:56 -0300

    refactor(service): migrate AnimeService from in-memory to JPA with DTOs

--- src/main/java/academy/devdojo/springboot_essentials/service/AnimeService.java
+++ src/main/java/academy/devdojo/springboot_essentials/service/AnimeService.java
@@ -1,6 +1,10 @@
 package academy.devdojo.springboot_essentials.service;
 
 import academy.devdojo.springboot_essentials.domain.Anime;
+import academy.devdojo.springboot_essentials.repository.AnimeRepository;
+import academy.devdojo.springboot_essentials.requests.AnimePostRequestBody;
+import academy.devdojo.springboot_essentials.requests.AnimePutRequestBody;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -10,38 +14,33 @@ import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 @Service
+@RequiredArgsConstructor
 public class AnimeService {
     // Responsavel pela lógica de négocio
-    // private final AnimeRepository animeRepository;
-    private static List<Anime> animes;
-
-    static {
-        animes = new ArrayList<>(List.of(new Anime(1L, "DBZ"), new Anime(2L, "Berserk")));
-    }
+    private final AnimeRepository animeRepository;
 
     public List<Anime> listAll()  {
-        return animes;
+        return animeRepository.findAll();
     }
 
-    public Anime findById(long id)  {
-        return animes.stream()
-                .filter(anime -> anime.getId().equals(id))
-                .findFirst()
+    public Anime findByIdOrThrowBadRequestException(long id)  {
+        return animeRepository.findById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
     }
 
-    public Anime save(Anime anime) {
-        anime.setId(ThreadLocalRandom.current().nextLong(3,1000000));
-        animes.add(anime);
-        return anime;
+    public Anime save(AnimePostRequestBody animePostRequestBody) {
+        return animeRepository.save(Anime.builder().name(animePostRequestBody.getName()).build());
     }
 
     public void delete(long id) {
-        animes.remove(findById(id));
+        animeRepository.delete(findByIdOrThrowBadRequestException(id));
     }
 
-    public void replace(Anime anime) {
-        delete(anime.getId());
-        animes.add(anime);
+    public void replace(AnimePutRequestBody animePutRequestBody) {
+        Anime savedAnime = findByIdOrThrowBadRequestException(animePutRequestBody.getId());
+        Anime anime = Anime.builder()
+                .id(animePutRequestBody.getId())
+                .name(animePutRequestBody.getName()).build();
+        animeRepository.save(anime);
     }
 }
