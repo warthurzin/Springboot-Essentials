a7ee86e338a55d5b77fe44a43d5064db97893111
Arthur Silveira <arthurmarques2148@gmail.com>
2025-10-20 10:47:24 -0300

    feat: transform Anime as JPA entity

--- src/main/java/academy/devdojo/springboot_essentials/domain/Anime.java
+++ src/main/java/academy/devdojo/springboot_essentials/domain/Anime.java
@@ -1,11 +1,19 @@
 package academy.devdojo.springboot_essentials.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.*;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Anime {
     // Representa o que temos do banco de dados
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 }
